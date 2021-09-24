5b84394f34006597b2ba7746d9c9bfe9f3cd2af3
Mikhail Nev <xp.play23@gmail.com>
2021-09-24 17:14:32 +0300

    начал беттинг делать

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/history',
+    name: 'History',
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/bet/:id',
+    name: 'Bet',
+    component: () => import('../views/Bet.vue')
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div v-for="(rate,index) in rates" :key="index">
+      <router-link :to="{ name: 'Bet', params: { id: rate.id }}">{{rate.name}}</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+export default {
+  name: 'Home',
+  data(){
+    return {
+      rates: null
+    }
+  },
+   mounted() {
+      axios
+        .post('http://devlink1.tk//bm/vue_lessons/betting_admin/index.php',{
+          action: 'history'
+        })
+        .then(response => {
+          this.rates = response.data;
+          console.log(this.rates)
+        });
+        
+    }
+}
+</script>
